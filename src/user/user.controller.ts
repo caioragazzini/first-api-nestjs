07798ab3cdf264f88578249ed7270db6378b8394
caiorolim <caiorolim.squadra@parceiro.afya.com.br>
2025-05-06 20:21:30 -0300

    Iniciando projeto NestJS

---
+++ src/user/user.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, ParseIntPipe } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+
+  @Put(':id')
+  async update(@Param('id') id, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Patch(':id')
+  async partialUpdate(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  return this.userService.update(+id,updateUserDto); // método separado no service
+}
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return(id);
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UserService {
+
+  create(createUserDto: CreateUserDto) {
+    return createUserDto;
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return {
+      message: `Usuário com id ${id} atualizado com sucesso`,
+      data: updateUserDto,
+    };
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
